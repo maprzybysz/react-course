22ac7558177fcb79c6be011a6dfa731b6558d78d
Mateusz Przybysz <notentego678@gmaill.com>
2021-11-20 14:47:33 +0100

    01.1-what-is-react-and-jsx

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const Paragraph = () => {
+	return React.createElement(
+		'p',
+		{
+			id: 'first-paragraph',
+			className: 'paragraph',
+		},
+		'Lorem ipsum dolor sit amet'
+	);
+};
+
+const appContainer = document.querySelector('#app');
+
+ReactDOM.render(React.createElement(Paragraph), appContainer);
