8cd32786962bfee4e74c428dfe082b3eae76ee84
Mateusz Przybysz <notentego678@gmaill.com>
2021-11-20 15:32:12 +0100

    02.1-configuration-modern-development

---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const data = [
+	{
+		name: 'Adam',
+		age: 28,
+		group: 'Sem I / Group B',
+	},
+	{
+		name: 'Roman',
+		age: 30,
+		group: 'Sem III / Group C',
+	},
+	{
+		name: 'Ania',
+		age: 27,
+		group: 'Sem I / Group A',
+	},
+	{
+		name: 'Karol',
+		age: 31,
+		group: 'Sem IV / Group A',
+	},
+];
+
+// export default data;
